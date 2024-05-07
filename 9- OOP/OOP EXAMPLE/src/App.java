6e317cc0677b8201b8b91f567224d715411f9a6c
recepzgrmh <recepzgrmh@gmail.com>
2024-05-07 18:16:25 +0300

    oop

--- 9- OOP/OOP EXAMPLE/src/App.java
+++ 9- OOP/OOP EXAMPLE/src/App.java
@@ -1,5 +1,59 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // fields: Fields represent attributes or properties of an object. They
+        // encapsulate the state of an object.
+
+        // object: An object is an instance of a class. It represents a specific entity
+        // or concept and contains its own state and behavior.
+
+        // method: A method is a function that is associated with a class or an object.
+
+        // It defines the behavior or actions that objects of a class can perform.
+
+        // Object Creation
+        // Here, we instantiate a Car object using the constructor method. The
+        // constructor
+        // initializes the object's state by setting values for its fields.
+
+        // Car car1 = new Car();
+
+        /*
+         * If fields were public variables, we could directly assign values like this:
+         * 
+         * car1.color = "blue"; car1.doors = 4; car1.model = "Renault Megane";
+         * car1.engine = 2.4;
+         * 
+         * However, accessing fields directly like this violates the encapsulation
+         * principle, as it exposes the internal state of the Car object. Encapsulation
+         * helps in maintaining data integrity and controlling access to the object's
+         * state.
+         * 
+         */
+
+        /*
+         * The above approach is not recommended because it exposes the internal state
+         * of the Car object and violates the encapsulation principle. Instead, we use
+         * setter methods to set the values of the fields, ensuring proper
+         * encapsulation and data abstraction.
+         * 
+         * car1.setColor("blue"); car1.setDoors(4); car1.setEngine(2.4);
+         * car1.setModel("Renault Megane");
+         * 
+         */
+
+        // Constructor Method
+        // Here, we create a new Car object using the constructor method. The
+        // constructor method initializes the object's state with the provided values
+        // for color, model, engine, and doors.
+
+        Car car1 = new Car("blue", "Renault Megane", 2.4, 4);
+
+        // Printing Car Information
+        // Here, we call the getInfo() method of the Car class to print information
+        // about the car, such as its model, color, number of doors, and engine
+        // specifications.
+
+        car1.getInfo();
     }
 }
--- 9- OOP/OOP EXAMPLE/src/Car.java
+++ 9- OOP/OOP EXAMPLE/src/Car.java
@@ -1,42 +1,101 @@
 public class Car {
 
+    // fields, properties
+
+    /*
+     * public String color;
+     * public String model;
+     * public double engine;
+     * public int doors;
+     *
+     * The usage isn't correct because fields should be private. If they were
+     * public, we could access them everywhere in the code, which doesn't fit with
+     * the encapsulation principle.
+     * 
+     */
+
+    private String color;
     private String model;
+    private double engine;
+    private int doors;
 
-    
-    public Car(){
-        model = "Boş";
-        color = "Yok";
-        speed = 0;
+    // Constructor Method
+    public Car(String color, String model, double engine, int doors) {
+        setColor(color);
+        setDoors(doors);
+        setEngine(engine);
+        setModel(model);
     }
 
-    public Car(String model, String color, int speed) {
+    // default constructor
+    public Car() {
+        /*
+         * this.color = "None";
+         * this.doors = 0;
+         * this.engine = 0;
+         * this.model = "None";
+         */
+
+        this("None", "None", 0, 0);
+    }
+
+    // Since we cannot access private variables in other files, we can define a
+    // public method. Then, we can access the fields through this method
+
+    public void setColor(String color) {
+        this.color = color; // "this" refers to the instance variable
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setEngine(double engine) {
+        this.engine = engine;
+    }
+
+    public double getEngine() {
+        return engine;
+    }
+
+    public void setModel(String model) {
         this.model = model;
-     
-
     }
 
-    private void setSpeed(int speed) {
-        if (this.speed < 0 || this.speed > 800) {
-            this.speed = 0;
+    public String getModel() {
+        return model;
+    }
+
+    public void setDoors(int doors) {
+        // The purpose of Encapsulation is to set some limits on assignments. For
+        // example, if a car does not have 3 doors, we only want it to have 4 or 2
+        // doors.
+        if (doors == 2 || doors == 4) {
+            this.doors = doors;
+        } else {
+            this.doors = 0;
+            System.out.println("The car must have 2 or 4 doors");
         }
     }
 
-    private int getSpeed() {
-        return speed;
-    } 
-
-    public void accalerate(int value){
-        speed += value;
-        System.out.println(model + " is accelerating. Current speed: " + speed);
+    public int getDoors() {
+        return doors;
     }
 
-    public void brake(int value){
-        speed = speed-value < 0 ? 0 : speed - value;
-        System.out.println(model + " is accelerating. Current speed: " + speed);
+    // method
+    public void start() {
+        System.out.println("Car was started");
+    }
+
+    public void stop() {
+        System.out.println("Car was stopped");
+    }
+
+    public void getInfo() {
+        System.out.println("Car's model " + getModel());
+        System.out.println("Car's color " + getColor());
+        System.out.println("Number of door in the car " + getDoors());
+        System.out.println("Car's engine " + getEngine());
     }
 
 }
-
-      
-
-       
