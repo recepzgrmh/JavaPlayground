4cc4dc152968d485bb0062d7365373808398b1ed
recepzgrmh <recepzgrmh@gmail.com>
2024-05-16 14:34:22 +0300

    registration file

---
+++ 11- RegistrationScreen/app/src/main/java/com/example/a11_registrationscreen/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.a11_registrationscreen;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    public void btnClick(View view){
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText email = findViewById(R.id.email);
+
+        TextView textFirstName = findViewById(R.id.textFirstName);
+        TextView textLastName = findViewById(R.id.textLastName);
+        TextView textEmail = findViewById(R.id.textEmail);
+
+        textFirstName.setText("First Name: " + firstName.getText().toString());
+        textLastName.setText("Last Name: " + lastName.getText().toString());
+        textEmail.setText("Email: " + email.getText().toString());
+    }
+}
