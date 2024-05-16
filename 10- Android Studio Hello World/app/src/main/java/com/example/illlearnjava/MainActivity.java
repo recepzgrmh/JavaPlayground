53d7204bb5a53ee89c59bf8d4f94d4a71687c3e7
recepzgrmh <recepzgrmh@gmail.com>
2024-05-16 13:56:53 +0300

    Android Studio First Test

---
+++ 10- Android Studio Hello World/app/src/main/java/com/example/illlearnjava/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.illlearnjava;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: complete this
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+
+    public void onBtnClick(View view){
+        TextView txtHello = findViewById(R.id.txtMessage);
+        EditText editText = findViewById(R.id.editName);
+        String txtEdit = editText.getText().toString();
+
+        txtHello.setText("Hello " + txtEdit + "!");
+    }
+}
